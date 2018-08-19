7348d4d7ff11b8d55a4366ca272650de8be1ae4d
Jernej Vivod <je.vivod@gmail.com>
2018-08-19 13:33:29 +0200

    Add examples

---
+++ Mathematical_Problems/exercises1.pl
@@ -0,0 +1,31 @@
+% Make a predicate s(N, N1), which is true if N1 = N + 1. The predicate shoudl work for all 4 possible situations:
+% As a check, as a successor generator, as a predecessor generator and as a generator of pairs of positive integers in relation s.
+
+% Make a predicate issum(A, B, Summa), which checks if A - B = Summa. You may only use the predicate s from the previous task. A and B can be
+% positive or equal to 0.
+
+% Make a predicate isdiff(A, B, Diff) which checks if A - B = Diff. 
+% You may only use the predicate s from the previous task. A is greater or equal to 0, B is less or equal to A.
+
+
+% similar for product (isprod(A, B, Prod))
+
+% similar for integer division (isdiv(A, B, Div))
+
+% Make a predicate num_digits(X, Digits) so that Digits is the numer of decimal digits of number X.
+% You may only use predicates from the previous tasks.
+
+% Make a predicate power(A, Pow, Res) so that Res is equal to A^Pow. Pow is an integer greater or equal to 0.
+% You may only use predicates from the previous tasks.
+
+% Make a predicate abs(X, AbsX) so that AbsX = |X|
+
+% Make a predicate sign(X, signX) so that signX is equal to 1 if X > 0, equal to 0 if X = 0 and equal to -1 if  X < 0.
+
+% Make a predicate is_prime(X) which checks if X is a prime number
+
+% Make a predicate not_prime(X) which checks if X has a divisor that is not equal to 1 or X (is not prime).
+
+% Make a program for converting roman numerals to arabic numerals.
+
+% Make a program for converting arabic numerals to roman numerals.
---
+++ databases/exercises1.pl
@@ -0,0 +1,48 @@
+% We are given the predicates lecture(Day, From, To, Class) and
+% tutorials(Day, From, To, Subject).
+
+% Ask Prolog the following questions:
+%
+% Which lectures do I have on friday?
+%
+% What schedule do I have on Tuesday?
+%
+% When do I have Programming 2 classes?
+%
+% Do I have any tutorials on friday?
+%
+
+% We are given an additional procedure professor(Professor, Class) and assistant(Assistant, Class).
+
+% Ask Prolog the following questions:
+%
+% Who has lectures for the Programming 2 class?
+%
+% Which classes does professor Polak have?
+%
+% When does Professor Delak have lectures?
+%
+% Who are the teachers for the Programming 2 class?
+%
+% Does assistant Kolar have lectures on friday?
+%
+
+% We are given procedures teacher(Teacher, Class), teaches(Teacher, Class), student(Student, Class).
+
+% Ask Prolog the following questions:
+%
+% Which teacher teaches mathematics in class 4.C?
+%
+% Which teacher teaches Slovenian?
+%
+% Which students are taught by teacher Mavec?
+%
+% Does the class 2.A have chemistry on their schedule?
+%
+% How many students in the school have music on their schedule?
+%
+% Which teachers teach more than 5 classes?
+%
+
+% Make a predicate have_class(Day, Hour) which is true if there are classes on day Day at Hour hour.
+% You may use predicates from previous exercises.
